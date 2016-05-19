5120ff633b4f04478c6a291d25dcb57fb0d7975a
pado <patrick.dobler@mimacom.com>
2016-05-19 11:19:14 +0200

    define the customAuthenticationProvider as a Bean to show the "new" behaviour in spring-security-4.1

--- sample-spring-boot-web/src/main/java/com/mimacom/app/springboot/web/config/CustomSecurityConfig.java
+++ sample-spring-boot-web/src/main/java/com/mimacom/app/springboot/web/config/CustomSecurityConfig.java
@@ -1,6 +1,7 @@
 package com.mimacom.app.springboot.web.config;
 
 import org.springframework.boot.autoconfigure.security.SecurityProperties;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.annotation.Order;
 import org.springframework.security.authentication.AuthenticationProvider;
@@ -27,7 +28,9 @@ public class CustomSecurityConfig extends WebSecurityConfigurerAdapter {
                 .antMatcher("/**").authorizeRequests().anyRequest().fullyAuthenticated();
     }
 
-    private AuthenticationProvider customAuthenticationProvider() {
+    // REMOVE THE @Bean annotation and you'll have two separated AuthenticationProviders and can access the actuator endpoints with admin/secret
+    @Bean
+    public AuthenticationProvider customAuthenticationProvider() {
         final DaoAuthenticationProvider daoAuthenticationProvider = new DaoAuthenticationProvider();
         daoAuthenticationProvider.setUserDetailsService(new MockedUserDetailsService());
         return daoAuthenticationProvider;
